781e3056109643a021b7bab8d5a1ed773a3b6224
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-06-26 19:19:27 +0600

    use bacis text animation and yoyo

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-gsap.from(".box1",{
+// gsap.from('h1',{
+//     opacity:0,
+//     duration:1,
+//     y:30,
+//     delay:1,
+//     stagger:0.5
+// }) -- text animation
+
+gsap.to(".box1",{
     x:1100,
     duration:2,
     delay:1,
     rotate:360,
-    backgroundColor:"blue",
-    borderRadius:'50%',
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
 })
