016e0266b44476def0423163b138d07c80af42e3
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-06-26 19:47:43 +0600

    use timeline()

--- script.js
+++ script.js
@@ -6,12 +6,56 @@
 //     stagger:0.5
 // }) -- text animation
 
-gsap.to(".box1",{
+// gsap.to(".box1",{
+//     x:1100,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     borderRadius:"50%",
+//     repeat:-1,
+//     yoyo:true
+// })
+
+// -----------------using timeline
+// gsap.to('.box1',{
+//     x:1100,
+//     duration:1.5,
+//     delay:1,
+//     rotate:360
+// })
+// gsap.to('.box2',{
+//     x:1100,
+//     duration:1.5,
+//     delay:2.5,
+//     rotate:360,
+//     borderRadius:'30%'
+// })
+// gsap.to('.box3',{
+//     x:1100,
+//     duration:1.5,
+//     delay:4,
+//     rotate:360,
+//     borderRadius:'50%'
+// })
+
+var tl = gsap.timeline()
+tl.to('.box1',{
     x:1100,
-    duration:2,
+    duration:1.5,
     delay:1,
+    rotate:360
+})
+tl.to('.box2',{
+    x:1100,
+    duration:1.5,
     rotate:360,
-    borderRadius:"50%",
-    repeat:-1,
-    yoyo:true
+    borderRadius:'30%'
+
+})
+tl.to('.box3',{
+    x:1100,
+    duration:1.5,
+    rotate:360,
+    borderRadius:'50%'
+
 })
